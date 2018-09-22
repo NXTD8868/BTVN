3b566afb03ecadef22eea7f68f44fd951429c33c
yoloteam456 <definitelynotduong@gmail.com>
2018-09-22 19:02:53 +0700

    .

---
+++ com_guestnum.py
@@ -0,0 +1,35 @@
+print("pick a number but dont tell me")
+print("A=more")
+print("B=less")
+x = input("is it  50?")
+while x not in ["A", "B"]:
+    x = input("only A or B")
+if x == "A":
+    x = input("is it  75")
+    if x == "A":
+        x = input("is it 88?")
+        if x == "A":
+            x = input("is it 94")
+            if x == "B":
+                x = input("is it 91")
+                if x == "A":
+                    
+            if x == "A":
+                x = input("is it 97")
+                if x =="B":
+                    x = input("is it 96")
+                    if x == "B":
+                        print("it is 95")
+                if x =="A":
+                    x = input("is it 99")
+                    if x == "B":
+                        print("it is 98")
+                    if x == "A":
+                        print("it is 100")
+                    
+                 
+
+    if y == "B":
+        z = input("is it 63")
+
+
